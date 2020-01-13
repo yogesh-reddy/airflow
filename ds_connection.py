50c5d094624e559fe87279eccf160ee378b5c13d
Yogesh <33508611+yogesh-reddy@users.noreply.github.com>
2020-01-13 15:32:12 +0530

    Create ds_connection.py

---
+++ ds_connection.py
@@ -0,0 +1,25 @@
+import time
+
+import paramiko
+import sys
+
+nbytes = 4096
+hostname = 'dgamonmdhds01.sl.bluecloud.ibm.com'
+port = 22
+username = 'airflow'
+password = 'Dec19Dec@12345!'
+command ='sh /home/dsadm/DataStage/MDHDEV/Script/run_datastage_script.sh MDHDEV Status_chk  /home/dsadm/DataStage/MDHDEV/data'
+try:
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.WarningPolicy)
+    print('Trying to Connect To Data Stage server')
+    client.connect(hostname, port=port, username=username, password=password)
+    print('Connected')
+    print('running the job')
+    stdin1, stdout1, stderr1 =client.exec_command('sh testrun.sh')
+    print(stdout1.read())
+    print('job ran')
+finally:
+    print('Clossing connection To Data Stage server')
+    client.close()
